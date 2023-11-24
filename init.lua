e67119eb9e3e6ecb3f667a764bbd0540ce7f3446
Yasniel <yasnielfajardoegues1408@gmail.com>
2023-11-24 13:24:58 -0300

    add cursor smooth

--- init.lua
+++ init.lua
@@ -18,20 +18,8 @@ else
   require("config.autocmds")
   require("config.keymaps")
 end
-require("rest-nvim").setup({
-  -- Open request results in a horizontal split
-  result_split_horizontal = false,
-  -- Keep the http file buffer above|left when split horizontal|vertical
-  result_split_in_place = false,
-  -- Skip SSL verification, useful for unknown certificates
-  skip_ssl_verification = false,
-  -- Highlight request on run
-  highlight = {
-    enabled = true,
-    timeout = 150,
-  },
-  -- Jump to request line on run
-  jump_to_request = false,
-  env_file = ".env",
-  yank_dry_run = true,
-})
+
+require("rest-nvim").setup()
+require("colorizer").setup()
+require("ccc").setup()
+require("smoothcursor").setup()
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,4 +1,6 @@
 return {
   { "folke/lazy.nvim", version = false },
   { "LazyVim/LazyVim", version = false },
+  { "yanick/nvim-jq", version = false },
+  { "uga-rosa/ccc.nvim" },
 }
